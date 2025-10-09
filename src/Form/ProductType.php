5e94e79954c32182c103998c73d082ccc098f45a
pinyink <nurkhafindi@gmail.com>
2025-10-09 16:05:10 +0700

    product

---
+++ src/Form/ProductType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Product;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+
+class ProductType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nama', TextType::class, [ 'label' => 'Nama', 'required' => true ])
+			->add('harga', IntegerType::class, [ 'label' => 'Harga', 'required' => true ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Product::class,
+            // enable/disable CSRF protection for this form
+            'csrf_protection' => true,
+            // the name of the hidden HTML field that stores the token
+            'csrf_field_name' => '_token',
+            // an arbitrary string used to generate the value of the token
+            // using a different string for each form improves its security
+            // when using stateful tokens (which is the default)
+            'csrf_token_id'   => 'user_item',
+        ]);
+    }
+}
